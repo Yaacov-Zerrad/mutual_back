8bbe44046c24fa8576bba11a0d430fdaa3352373
Debian <admin@ip-172-31-17-206.ec2.internal>
2022-09-17 20:05:00 +0000

    1 servr

--- core/core/settings.py
+++ core/core/settings.py
@@ -87,13 +87,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE':'django.db.backends.postgresql_psycopg2',
+		'NAME': 'mutual',
+		'USER':'yaacov',
+		'PASSWORD':'20Myriam',
+		'HOST':  'localhost',
+		'POST':'5432',
+	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
